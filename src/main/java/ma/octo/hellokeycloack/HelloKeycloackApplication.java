692a54c85f5d37570168ad3daab13abdcba97d91
Chaimae-Bouhouch <75264754+Chaimae-Bouhouch@users.noreply.github.com>
2022-03-08 20:14:22 +0100

    first commit

---
+++ src/main/java/ma/octo/hellokeycloack/HelloKeycloackApplication.java
@@ -0,0 +1,13 @@
+package ma.octo.hellokeycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKeycloackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloKeycloackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/octo/hellokeycloack/HelloKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.octo.hellokeycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKeycloackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
